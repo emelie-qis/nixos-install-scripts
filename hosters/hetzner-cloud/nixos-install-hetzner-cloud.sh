e03f41e5ef17752d90a07b8b734775060e47ef1b
Emelie <em@nao.sh>
2021-08-05 14:37:00 +0200

    Replace pubkey

--- hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
+++ hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
@@ -66,7 +66,7 @@ echo '
 
   users.users.root.openssh.authorizedKeys.keys = [
     # Replace this by your SSH pubkey!
-    "ssh-rsa AAAAAAAAAAA..."
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICO4LyBsW1YuUA6i3EL/IZhchSvk7reO4qgRmR/tdQPU id_ed25519"
   ];
 }
 ' >> /mnt/etc/nixos/configuration.nix
